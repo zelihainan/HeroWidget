7e0a74967964d7849be56b23ffbc5d655bf80ddd
Zeliha İnan <zelihainann@gmail.com>
2025-08-28 01:07:05 +0300

    Initial Commit

---
+++ HeroWidget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HeroWidget
+//
+//  Created by Zeliha İnan on 28.08.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HeroWidget/HeroWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroWidgetApp.swift
+//  HeroWidget
+//
+//  Created by Zeliha İnan on 28.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct HeroWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
